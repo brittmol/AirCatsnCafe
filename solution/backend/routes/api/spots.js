5a2c957b83bfed7a13912c805f7db298e9b13062
Brittany Moliver <britt.moliver@gmail.com>
2022-02-15 17:42:42 -0700

    added routes to spots.js

--- solution/backend/routes/api/spots.js
+++ solution/backend/routes/api/spots.js
@@ -12,10 +12,58 @@ const router = express.Router();
 
 // ------------------ ROUTES ------------------------------------------
 
-router.get("/", asyncHandler(async(req, res) => {
-    const spots = await Spot.findAll()
-    return res.json(spots)
-}))
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const spots = await Spot.findAll();
+    return res.json(spots);
+  })
+);
+
+router.post(
+  "/",
+  requireAuth,
+  validateEvent,
+  asyncHandler(async (req, res) => {
+    const spot = await Spot.create(req.body);
+    return res.json(spot);
+  })
+);
+
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const spot = await Spot.findByPk(req.params.id, {
+      include: [User],
+    });
+    return res.json(spot);
+  })
+);
+
+router.put(
+  "/:id",
+  requireAuth,
+  validateEvent,
+  asyncHandler(async (req, res) => {
+    const spot = await Spot.findByPk(req.params.id);
+    const updatedSpot = await spot.update(req.body);
+    const updatedSpotx = await Spot.findByPk(updatedSpot.id, {
+      include: [User],
+    });
+    return res.json(updatedSpotx);
+  })
+);
+
+router.delete(
+  "/:id",
+  requireAuth,
+  asyncHandler(async (req, res) => {
+    const spot = await Spot.findByPk(req.params.id);
+    if (!spot) throw new Error("Cannot find spot");
+    await spot.destroy();
+    return res.json({});
+  })
+);
 
 // ------------------ EXPORTS ------------------------------------------
 module.exports = router;
